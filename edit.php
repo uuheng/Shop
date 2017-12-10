c70b5cd433f9bbd309252374fc7a68aa25ee12de
uheng <qyffxzh@126.com>
2017-12-10 16:13:43 +0800

    test

--- edit.php
+++ edit.php
@@ -6,12 +6,12 @@
     <body>
         <center>
             <?php
-
+                echo "safdsadf";
 
              ?>
              <h3>编辑商品信息</h3>
              <form action="action.php?action=update" enctype="multipart/form-data" method="post">
-                 
+
              </form>
         </center>
     </body>
